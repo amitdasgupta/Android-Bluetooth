5557be4c8f02ff54a71585ac7c88921a070b7192
Akshay Khatter <30326830+akshaykhatter@users.noreply.github.com>
2017-10-06 22:16:46 +0530

    Update MainActivity.java

--- app/src/main/java/com/example/amitdasgupta/bluetooth/MainActivity.java
+++ app/src/main/java/com/example/amitdasgupta/bluetooth/MainActivity.java
@@ -5,6 +5,12 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import akshaykhatter;
 
 public class MainActivity extends AppCompatActivity {
 
